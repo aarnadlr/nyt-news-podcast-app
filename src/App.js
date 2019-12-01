fc352896bcb66cd4862b7c8d779f3a015b2444a2
aarnadlr <aaronadler@gmail.com>
2019-12-01 15:54:50 -0500

    convert fetch to async/await

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { useSpeechSynthesis } from 'react-speech-kit';
 
 function App() {
-
   const [totalNumOfArticles, setTotalNumOfArticles] = useState(null);
 
-
-  const [longTranscriptString, setLongTranscriptString] = useState('Loading...');
+  const [longTranscriptString, setLongTranscriptString] = useState(
+    'Loading...'
+  );
 
   // SELECT WHICH VOICE by index
   const [voiceIndex] = useState(7);
@@ -16,45 +16,54 @@ function App() {
   // SELECTED VOICE
   const voice = voices[voiceIndex] || null;
 
+  let finalSingleString = '';
 
-  useEffect(() => {
 
-    let finalSingleString = '';
+  const buildString = (data) => {
+    // Save the total num of articles (to render separately)
+    setTotalNumOfArticles(data.results.length);
 
-    fetch(
+    let podcastIntroString = `Hello. And welcome to The Daily Review Podcast. We are pleased to present you the top ${data.results.length} stories from The New York Times:`;
+
+    // Store the long final accum string
+    finalSingleString = data.results.reduce(
+      (accumString, currObj, ind, srcArr) => {
+        let prefix = `Story number ${ind + 1}.`;
+
+        let title = `${currObj.title}.`;
+        let abstract = currObj.abstract;
+
+        //Build the single string for each articleObj:
+        // concat the prefix, the title, the abstract
+        let singleString = prefix.concat(' ', title).concat(' ', abstract);
+
+        // Add to the accumString the single string
+        return accumString.concat(' ', singleString);
+      },
+      ''
+    );
+    let introWithLongString = podcastIntroString.concat(finalSingleString);
+    //Take the finalSingleString and save it to state
+    setLongTranscriptString(introWithLongString);
+  };
+
+
+
+  useEffect(async () => {
+    // 1. store the response
+    const response = await fetch(
       'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=YFNhGhkzEe0Yq3KwV25z5b7qNvzGYmne'
-    )
-      .then(res => res.json())
-      .then(data => {
+    );
+    // 2. store the converted JSON
+    const myJson = await response.json();
+    // 3. Do stuff
+    buildString(myJson);
 
-        // Save the total num of articles (to render separately)
-        setTotalNumOfArticles(data.results.length);
 
-        let podcastIntroString = `Hello. And welcome to The Daily Review Podcast. We are pleased to present you the top ${data.results.length} stories from The New York Times:`;
 
-        // Store the long final accum string
-        finalSingleString =
-          data.results.reduce(
-            (accumString, currObj, ind, srcArr) => {
-              let prefix = `Story number ${ind + 1}.`;
+  }, []); //<--END OF UseEffect
 
-              let title = `${currObj.title}.`;
-              let abstract = currObj.abstract;
 
-              //Build the single string for each articleObj:
-              // concat the prefix, the title, the abstract
-              let singleString = prefix.concat(' ', title).concat(' ', abstract);
-
-              // Add to the accumString the single string
-              return accumString.concat(' ', singleString);
-            }, ''
-          );
-        let introWithLongString = podcastIntroString.concat(finalSingleString);
-        //Take the finalSingleString and save it to state
-        setLongTranscriptString(introWithLongString);
-      });  //<--END OF THEN BLOCK
-
-  }, []);
 
   return (
     <div
@@ -82,7 +91,7 @@ function App() {
         type={'primary'}
         onClick={() => speak({ text: longTranscriptString, voice })}
       >
-        <span role="img" >▶</span>️ PLAY The Daily Review
+        <span role="img">▶</span>️ PLAY The Daily Review
       </button>
 
       <br />
